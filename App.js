0755e4926191ffe661e95a0774e26ff78c035aad
Mukesh Kumar <mukeshsharma1201@gmail.com>
2019-08-29 19:12:42 +0530

    inital commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import {StyleSheet, View, Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
